ae8d0b264596b4b934b64013941072003077816f
Dinusha <dnsw83@gmail.com>
2016-02-06 23:45:07 +0530

    fist commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+export default class Layout extends React.Component {
+
+  render() {
+    return (
+      <div>
+       <h1>Welcome to the React World</h1>
+ 
+      </div>
+    );
+  }
+}
